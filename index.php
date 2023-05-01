a09602dd02e51a911651d4cf13f87784c84b74ea
Jaron Avittan <96601217+jaronavittan@users.noreply.github.com>
2023-05-01 15:37:27 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<link rel="stylesheet" href="stylesheet.css">
+<h1>This is your Company Greeting!</h1>
+<p>Some text can be added here,</p>
+<p>Some more text here.</p>
+<img src="https://th.bing.com/th/id/OIP._Dthq0KR-DkXt8sQlaaSmgAAAA?w=238&h=180&c=7&r=0&o=5&pid=1.7" class="center">  
+<footer><?php include 'footer.php';?></footer>
+
+</body>
+</html>
